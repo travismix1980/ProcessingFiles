95115f349d80b6f311157a36b758cbece985dfeb
Travis Mix <travismix1980@gmail.com>
2023-07-25 04:43:04 -0700

    Updated to use Try With Resources to be more modern

--- src/com/travis/learning_file_reader/Application.java
+++ src/com/travis/learning_file_reader/Application.java
@@ -5,10 +5,7 @@ import java.io.*;
 public class Application {
     public static void main(String[] args) throws IOException {
         File file = new File("myFile.txt");
-        BufferedReader br = null;
-        try {
-            FileReader fr = new FileReader(file);
-            br = new BufferedReader(fr);
+        try(FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
             String line = br.readLine();
             while(line != null){
                 System.out.println(line);
@@ -18,9 +15,6 @@ public class Application {
             e.printStackTrace();
         } catch(IOException e){
             throw new IOException(e);
-        } finally {
-            assert br != null;
-            br.close();
         }
     }
 }
